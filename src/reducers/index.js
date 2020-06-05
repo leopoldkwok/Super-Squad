24ed141b3bd59efcae02a3f84a6d2bc869450535
leopoldkwok <leopoldkwok@gmail.com>
2020-06-05 19:24:00 +0100

    Adding a Second Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,11 @@
-import characters_json from "../data/characters.json";
-import { ADD_CHARACTER } from "../actions";
+import { combineReducers } from "redux";
 
-function characters(state = characters_json, action) {
-  switch (action.type) {
-    case ADD_CHARACTER:
-      let characters = state.filter((item) => item.id !== action.id);
-      return characters;
-    default:
-      return state;
-  }
-}
+import characters from "./characters_reducer";
+import heroes from "./heroes_reducer";
 
-export default characters;
+const rootReducer = combineReducers({
+  characters,
+  heroes,
+});
+
+export default rootReducer;
